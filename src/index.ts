199aaa4deb47c73013b8f528a3b52f64fb01e51c
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2024-11-15 11:27:40 +0100

    Utilisation de la balise canvas

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,39 @@
-class Test
-{
-    protected message: string ;
-
-    constructor( msg: string )
-    {
-        this.message = msg ;
-    }
-
-    public afficheMessage(): void
-    {
-        console.log( "Le message est: " + this.message ) ;
-    }
-
-    private toto(): void
-    {
-
-    }
-
-}
+import {Test} from "./Test"
 
 let message: Test ;
 message = new Test( "coucou") ;
 message.afficheMessage() ;
+
+let canvas = document.querySelector( "canvas") ;
+let ctx = canvas.getContext( "2d" ) ;
+
+//ctx.rotate( Math.PI/2 ) ;
+//ctx.translate( 100, 100 ) ;
+//ctx.rotate( Math.PI/2 ) ;
+
+// Dessin d'un rectangle colorié en bleu ciel
+ctx.fillStyle = "#00FFF0" ;
+ctx.fillRect( 0, 0, 100, 50 ) ;
+
+// Dessin d'une droite
+ctx.strokeStyle = "red" ;
+
+ctx.beginPath() ;
+ctx.moveTo( 100, 100 ) ;
+ctx.lineTo( 200, 180 ) ;
+ctx.lineTo( 200, 80 ) ;
+ctx.stroke() ;
+
+// Translation du repère d'axes
+ctx.translate( 200, 150 ) ;
+
+// Dessin d'un rectangle colorié en bleu ciel
+ctx.fillStyle = "#0FFFF0" ;
+ctx.fillRect( 0, 0, 100, 50 ) ;
+
+// Translation du repère d'axes
+ctx.rotate( Math.PI/4 ) ;
+
+// Dessin d'un rectangle colorié en bleu ciel
+ctx.fillStyle = "#F0FF00" ;
+ctx.fillRect( 0, 0, 100, 50 ) ;
