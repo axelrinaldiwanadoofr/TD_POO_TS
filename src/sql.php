da4c94a76be20447de847c3b02dbced797e1da30
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2025-01-24 11:47:14 +0100

    Tentative de transfert de clé et iv de js à php: bug

--- src/sql.php
+++ src/sql.php
@@ -7,9 +7,15 @@
 
 // Test de chiffrement / déchiffrement
 
+$keyJsStringHex = "d9f7340c9bc71ffe6075be002dfcccd5" ;
+$ivJsStringHex = "4eb1b0a59e32f28dc4d9fa8c93d6fe4e" ;
+$resultatChiffreJsStringHex = "bb8a067d49d0b393a67f8a17f5eb0b6583219ad3bca16e07c9e3a84a8a152feef44b1270cbac7c36a76afde3076317d85fab17cf5d67b9115b0640ade5c9701f" ;
+
+
 $algo_chiffrement_dechiffrement = "aes-128-cbc" ;
-$key = "Ceci est ma clé " ;
-$iv =  "Ceci est mon iv_" ;
+
+$key = hex2bin( $keyJsStringHex ) ;
+$iv = hex2bin( $ivJsStringHex ) ;
 
 $message = "Bonjour, nous allons faire un test de chiffrement d'un message" ;
 
@@ -28,7 +34,10 @@ if( in_array( $algo_chiffrement_dechiffrement, $les_algo_dispo ) )
         $iv ) ;
 
     echo "Message chiffré: " . $messageChiffre . "<br>" ;
+    echo "Message chiffré string hex:<br>" . bin2hex( base64_decode( $messageChiffre) . "<br>" ) ;
+    echo "Message chiffré en js: <br>" . $resultatChiffreJsStringHex . "<br>";
 
+    $messageChiffre = hex2bin( $resultatChiffreJsStringHex . "3c62723e" ) ;
     $messageOriginal = openssl_decrypt( 
         $messageChiffre,
         $algo_chiffrement_dechiffrement,
@@ -59,7 +68,4 @@ $curseur = $pdo->query( $sql ) ;
 
 $resultat = $curseur->fetchAll( PDO::FETCH_ASSOC ) ;
 
-echo json_encode( $resultat ) ;
-*/
-
-?>
+echo json_encode( $resultat )
