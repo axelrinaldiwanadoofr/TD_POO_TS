cad4678d6575229ecc90a7fd689ac48a13b3bf77
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2026-09-22 17:59:03 +0200

    promise 1

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,16 @@ let compteur = 0 ;
 
 console.log( "time 0: compteur: " + compteur ) ;
 
-setTimeout( function ()
+let unePromise = new Promise( (resolve: any,reject: any ) =>
+{
+    setTimeout( function ()
+    {
+        resolve( "R1" ) ;
+    }, 2000 ) ;
+} ).then( (value ) =>
 {
     compteur++ ;
-    console.log( "time 1 compteur: " + compteur ) ;
-
-    setTimeout( function()
-    {
-        compteur++ ;
-        console.log( "time 2 compteur: " + compteur ) ;
-   
-        setTimeout( function()
-        {
-            compteur++ ;
-            console.log( "time 3 compteur: " + compteur ) ;
-        }, 4000 ) ;
-    }, 3000 ) ;
-}, 2000 ) ;
+    console.log( "time 1: compteur: " + compteur + " value: " + value ) ;
+})
 
 console.log( "fin" ) ;
