26473d7f0afed43bc334401dea9cb2287bd45ece
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2026-09-22 17:36:38 +0200

    Timeout 1

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,10 @@
 import { VwDessin } from "./VwDessin";
-import { Dessin } from "./Dessin" ;
-
-async function chargeListeDessin(): Promise<Array<Map<string,any>>>
-{
-    let url = "../src/php/dessins/listeDessin.php" ;
-    let response = await fetch( url ) ;
-    let data = await response.json() as Array<Map<string,any>> ;
-    return data ;
-}
-
-async function updateListeDessin(): Promise<void>
-{
-    let listeDessin = document.getElementById( "listeDessin" ) as HTMLSelectElement ;
-    let data = await chargeListeDessin() ;
-
-    let html = "" ;
-    data.forEach( (dessinData)=>
-    {
-        html += "<option value=" + dessinData["nom"] + ">" + dessinData["nom"] + "</option>" ;
-    }) ;
-    listeDessin.innerHTML = html ;
-}
 
 customElements.define( "vw-dessin", VwDessin ) ;
 
-updateListeDessin() ;
+console.log( "time 0" ) ;
 
-document.getElementById( "chargeDessin")?.addEventListener( "click", async (event)=>
+setTimeout( ()=>
 {
-    let listeDessin = document.getElementById( "listeDessin" ) as HTMLSelectElement ;
-    let vwDessin = document.getElementById( "vwDessin") as VwDessin ;
-
-    if( listeDessin.value )
-    {
-        let url = "../src/php/dessins/chargeDessin.php?nom=" + listeDessin.value ;
-        let response = await fetch( url ) ;
-        let data = await response.json() as Array<Map<string,any>> ;
-        
-        if( data.length )
-        {
-            let dessin = new Dessin() ;
-            dessin.createFiguresFromData( data ) ;
-            vwDessin.setDessin( dessin ) ;
-            return true ;
-        }
-    }
-    return false ;
-}) ;
-
+    console.log( "time 1" ) ;
+}, 2000 ) ;
