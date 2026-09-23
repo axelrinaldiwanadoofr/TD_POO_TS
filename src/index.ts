6518ea95a34eba456d913c1296a736b4253cb96c
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2026-09-23 08:38:20 +0200

    Traitement asynchrone avec fonction et promise

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,22 @@ let compteur = 0 ;
 
 console.log( "time 0: compteur: " + compteur ) ;
 
-let unePromise = new Promise( (resolve: any,reject: any ) =>
-{
-    setTimeout( function ()
-    {
-        resolve( "R1" ) ;
-    }, 2000 ) ;
-})
-.then( (value ) =>
-{
-    compteur++ ;
-    console.log( "time 1: compteur: " + compteur + " value: " + value ) ;
 
+// Fonction asynchrone 1
+function Async2000_R1(): Promise<string>
+{
+    return new Promise( (resolve: any,reject: any ) =>
+    {
+        setTimeout( function ()
+        {
+            resolve( "R1" ) ;
+        }, 2000 ) ;
+    }) ;
+}
+
+// Fonction asynchrone 2
+function Async3000_R2(): Promise<string>
+{
     return new Promise( (resolve: any,reject: any ) =>
     {
         setTimeout( function ()
@@ -25,25 +29,39 @@ let unePromise = new Promise( (resolve: any,reject: any ) =>
             resolve( "R2" ) ;
         }, 3000 ) ;
     }) ;
-})
-.then( (value)=>
-{
-    compteur++ ;
-    console.log( "time 2: compteur: " + compteur + " value: " + value ) ;
+}
 
+// Fonction asynchrone 3
+function Async4000_R3(): Promise<string>
+{
     return new Promise( (resolve: any,reject: any ) =>
     {
         setTimeout( function ()
         {
             resolve( "R3" ) ;
         }, 4000 ) ;
-    });
+    }) ;
+}
+
+// Tratitement asynchrone complet
+Async2000_R1().then( (value)=>
+{
+    compteur++ ;
+    console.log( "time 1: compteur: " + compteur + " value: " + value ) ;
+}).then( ()=>
+{
+    return Async3000_R2() ;
+}).then( (value)=>
+{
+    compteur++ ;
+    console.log( "time 2: compteur: " + compteur + " value: " + value ) ;
+}).then( ()=>
+{
+    return Async4000_R3() ;
 }).then( (value)=>
 {
     compteur++ ;
     console.log( "time 3: compteur: " + compteur + " value: " + value ) ;
-});
-
-
+}) ;
 
 console.log( "fin" ) ;
